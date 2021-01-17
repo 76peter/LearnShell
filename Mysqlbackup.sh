62efc5e4c32ede921a12b581078dda85565dd0bc
76peter <412044923@qq.com>
2021-01-17 23:31:05 +0800

    修改-exec异常
    find: missing argument to `-exec'

--- Mysqlbackup.sh
+++ Mysqlbackup.sh
@@ -7,6 +7,6 @@ time=` date +%Y%m%d%H `
 /opt/mysql/mysqldump -uroot --password=123456 -P3307 dataname | gzip > $backupdir/dataname_$time.sql.gz
 /opt/mysql/mysqldump -uroot --password=123456 -P3307 dataname1 | gzip > $backupdir/dataname_1$time.sql.gz
 #删除七天前的备份
-find $backupdir -name "dataname_*.sql.gz" -type f -mtime +7 -exec rm {} ; > /dev/null 2>&1
+find $backupdir -name "dataname_*.sql.gz" -type f -mtime +7 -exec rm { } \; > /dev/null 2>&1
 #
 #
